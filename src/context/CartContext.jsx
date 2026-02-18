da3057d9a52fe204ce2a36c319b0d7b01c0e7e7f
Ayush Gupta <ayushgupta353srplatinum@gmail.com>
2026-02-18 22:45:12 +0530

    Add files via upload

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,1 +1,19 @@
+import { createContext, useState } from "react";
 
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart((prev) => [...prev, product]);
+  };
+
+  const totalPrice = cart.reduce((acc, item) => acc + item.price, 0);
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, totalPrice }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
